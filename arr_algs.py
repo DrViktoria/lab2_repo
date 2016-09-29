fd02817d461ec15ab4a8a9ae3d5181ef719277d2
DrViktoria <viktoria.dr15@gmail.com>
2016-09-29 23:12:42 +0300

    Update arr_algs.py

--- arr_algs.py
+++ arr_algs.py
@@ -1,8 +1,4 @@
 arr = [5,9,1,2,45,3,8]
-s=arr[0]
-l= len(arr)
-#print(l)
-#print(s)
 
 def min(m):
 	k = m[0]
@@ -10,11 +6,8 @@ def min(m):
 		if el < k:
 			k=el
 	return k
-			
-			
-			
+				
 print(min(arr))	
-
 
 def arg(f):
 			s=0
